20a140ffac6ae770ae877096591d6b4ae1e953e8
cparandojo <carlosperezarandojo@gmail.com>
2018-05-26 18:23:04 +0200

    probando heroku

---
+++ src/router.js
@@ -0,0 +1,126 @@
+
+const express = require('express');
+const { fork } = require('child_process');
+
+const addUserProcessUrl = 'process/user/addUserProcess.js';
+const updateUserProcessUrl = 'process/user/updateUserProcess.js';
+
+//importamos solo las funciones del modelo que vamos a usar desde el router.
+const {getUsers, getUserById, deleteUser, deleteAll} = require('./model/userModel.js');
+
+const router = express.Router();
+
+//creación del usuario con proceso hijo.
+router.post('/user',(req,res)=>{
+    
+    //obtenemos el campo body de la petición
+    let data = req.body;
+
+    //Obtenemos el proceso hijo
+    const addUserProcess = fork(addUserProcessUrl);
+
+    //añadimos un evento al proceso hijo, para que envie los datos del json de respuesta.
+    addUserProcess.on('message', (responseBBDD) => {
+        res.status(201).json(responseBBDD);
+    });
+
+    addUserProcess.on('exit', () => {
+        //Respondemos con OK
+        res.status(500).json({error:'Error creando usuario.'});
+       
+    });
+
+    //ejecutamos el proceso.
+    addUserProcess.send(data);
+   
+});
+
+//listado de usuarios sin proceso hijo
+router.get('/users', (req, res)=>{
+
+    //obtenemos los resultados.
+    getUsers().then((data)=>{
+        console.log('Lista de usuarios obtenida.');
+
+        res.status(200).json(data);
+
+    }).catch((err) => {
+        console.log('Error obteniendo lista de usuarios');
+        console.log(err);
+        res.status(500).json({success:false});
+    });
+});
+
+//busqueda de usuario por id sin proceso hijo
+router.get('/users/:id', (req, res)=>{
+
+    let userId = req.params.id;
+
+    getUserById(userId).then((data)=>{
+        console.log('Usuario obtenido correctamente.');
+
+        res.status(200).json(data);
+
+    }).catch((err) => {
+        console.log('Error obteniendo usuario');
+        console.log(err);
+        res.status(500).json({success:false});
+    });
+});
+
+
+//actualización de usuario con proceso hijo.
+router.patch('/users/:id', (req, res)=>{
+   
+    
+    let data = req.body;
+    data.id = req.params.id;
+
+    //realizamos llamada al proceso hijo.
+    const updateUserProcess = fork(updateUserProcessUrl);
+   
+    //añadimos un evento al proceso hijo, para que envie los datos del json de respuesta.
+    updateUserProcess.on('message', (responseUpdateBBDD) => {
+        //Respondemos con OK
+         res.status(201).json(responseUpdateBBDD);
+    });
+
+    updateUserProcess.on('exit', () => {
+        //Respondemos con OK
+        res.status(500).json({error:'Error actualizando usuario.'});
+       
+    });
+
+    updateUserProcess.send(data);   
+   
+});
+
+//eliminación de usuario sin proceso hijo.
+router.delete('/users/:id', (req, res)=>{
+   
+    let data = req.body;
+    data.id = req.params.id;
+    
+    req.app.get('deleteUserProcess').send(data);
+
+    res.status(200).json(data);  
+});
+
+//eliminación de todos los usuarios
+router.delete('/users', (req, res)=>{
+   
+    let userId = req.params.id;
+    
+    deleteAll().then((data)=>{
+        console.log('Usuarios borrados correctamente')
+        res.status(200).json({success:true});
+
+    }).catch((err) => {
+        console.log('Error borrando usuarios');
+        console.log(err);
+        res.status(500).json({success:false});
+    });   
+});
+
+
+module.exports = router;
---
+++ src/server.js
@@ -0,0 +1,38 @@
+
+/*
+ * Importamos las liberias necesarias
+ * 
+ */
+
+const express = require('express');//facilita el maneja de las peticiones http, router
+const bodyParser = require('body-parser');
+
+//con esta instrucción, lo que realizamos es la importación solo de fork de todo lo que contiene el paquete child_process
+const { fork } = require('child_process')
+
+//importamos nuestro fichero router que contiene la definición de las url para la petición http.
+const router = require('./router.js');
+
+//indicamos el puerto en el que queremos que nuestro servidor este escuchando.
+const port = 3000;
+
+//Indicamos el archivo que sera nuestro proceso hijo.
+const deleteUserProcessUrl = 'process/user/deleteUserProcess.js';
+
+//creamos nuestra variable de aplicación.
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+
+//creamos el proceso hijo con fork, que nos retorna una instancia de dicho proceso.
+const deleteUserProcess = fork(deleteUserProcessUrl);
+
+//indicamos la url base sobre la que va a atender peticiones nuestro servidor.
+app.use('/api/v1/', router);
+
+//le añadimos nuestro proceso hijo bajo el nombre de userProcess
+app.set('deleteUserProcess', deleteUserProcess);
+
+app.listen(port);
